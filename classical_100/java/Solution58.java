3ddf5555c9c5cfd2a4c82321c166e659c1d8b33a
KJay <snoopy_zj@163.com>
2024-03-05 19:40:10 +0800

    最后一个单词的长度

---
+++ classical_100/java/Solution58.java
@@ -0,0 +1,23 @@
+package classical_100.java;
+
+public class Solution58 {
+    public int lengthOfLastWord(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+        for (int i = s.length()-1; i>=0; i--){
+            if(s.charAt(i) == ' '){
+                if (count > 0) return count;
+            }
+            else count++;
+        }
+
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "Hello World";
+        Solution58 solution = new Solution58();
+        System.out.println(solution.lengthOfLastWord(s));
+    }
+}
