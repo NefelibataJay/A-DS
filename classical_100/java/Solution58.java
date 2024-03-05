e4b064d70d02ca465899da77663ff3cf769e6a17
KJay <snoopy_zj@163.com>
2024-03-05 19:40:21 +0800

    最后一个单词的长度

--- classical_100/java/Solution58.java
+++ classical_100/java/Solution58.java
@@ -2,7 +2,6 @@ package classical_100.java;
 
 public class Solution58 {
     public int lengthOfLastWord(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
         int count = 0;
         for (int i = s.length()-1; i>=0; i--){
             if(s.charAt(i) == ' '){
