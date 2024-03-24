e705efb76f7cdee20933e234c79bae08c7f537dc
KJay <snoopy_zj@163.com>
2024-03-24 16:57:42 +0800

    两数相加

---
+++ classical_150/java/linklist/Solution2.java
@@ -0,0 +1,63 @@
+package classical_150.java.linklist;
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 逆序
+        ListNode head = new ListNode();
+        ListNode y = head;
+        ListNode p = l1;
+        ListNode q = l2;
+        int reduce = 0;
+
+        while (p != null && q != null) {
+            int sum = q.val + p.val + reduce;
+            reduce = sum / 10;
+            y.next = new ListNode(sum % 10);
+            y = y.next;
+            p = p.next;
+            q = q.next;
+        }
+
+        while (p != null) {
+            int sum = p.val + reduce;
+            reduce = sum / 10;
+            y.next = new ListNode(sum % 10);
+            y = y.next;
+            p = p.next;
+        }
+        while (q != null) {
+            int sum = q.val + reduce;
+            reduce = sum / 10;
+            y.next = new ListNode(sum % 10);
+            y = y.next;
+            q = q.next;
+        }
+
+        if (reduce != 0) {
+            y.next = new ListNode(reduce);
+        }
+
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
