0505ed3ecfcc1e92df231faa09354dc9a65cfbc6
KJay <snoopy_zj@163.com>
2024-04-26 14:34:11 +0800

    打家劫舍

--- classical_150/java/dynamicPlanning/Solution198.java
+++ classical_150/java/dynamicPlanning/Solution198.java
@@ -3,9 +3,6 @@ package classical_150.java.dynamicPlanning;
 public class Solution198 {
     public int rob(int[] nums) {
         // f(x) = Max(f(x - 2) + C, f(x-1))
-        int n = nums.length;
-        int[][] money = new int[n][2]; // [每个房子][偷了的话, 不偷的话]
-
         int x1 = nums[0], x2 = 0;
         for (int i = 1; i < nums.length; i++) {
             int x = Math.max(x2 + nums[i], x1);
