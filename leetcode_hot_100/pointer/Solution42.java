6b5bf132584782fd388d17a6288c8d1b3b6a5305
Zhou Jie <snoopy_zj@163.com>
2024-05-20 18:15:32 +0800

    接雨水

---
+++ leetcode_hot_100/pointer/Solution42.java
@@ -0,0 +1,33 @@
+package leetcode_hot_100.pointer;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        int[] height = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        System.out.println(trap(height));
+    }
+
+    public static int trap(int[] height) {
+        int n = height.length;
+        if (n < 3)
+            return 0;
+
+        int rain = 0;
+        int[][] dp = new int[n][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], height[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][1] = Math.max(dp[i + 1][1], height[i + 1]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int h = Math.min(dp[i][0], dp[i][1]);
+            if (h > height[i]) {
+                rain += h - height[i];
+            }
+        }
+
+        return rain;
+    }
+}
