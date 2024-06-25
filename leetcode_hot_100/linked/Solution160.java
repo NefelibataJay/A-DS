a28c8fb728be63a48aef2fd5bfdc71d54644faa4
KJay <snoopy_zj@163.com>
2024-06-25 13:13:01 +0800

    相交链表

--- leetcode_hot_100/linked/Solution160.java
+++ leetcode_hot_100/linked/Solution160.java
@@ -41,12 +41,12 @@ public class Solution160 {
     }
 
     public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) return null;
+        if (headA == null || headB == null)
+            return null;
         ListNode pA = headA, pB = headB;
-        while(pA != pB){
+        while (pA != pB) {
             pA = pA == null ? headB : pA.next;
             pB = pB == null ? headA : pB.next;
-            if (pA != null && pB != null)
         }
         return pA;
     }
