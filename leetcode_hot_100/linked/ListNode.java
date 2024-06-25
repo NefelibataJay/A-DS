a16fa84718d19900a6047204412389856d60ad95
KJay <snoopy_zj@163.com>
2024-06-25 16:16:40 +0800

    反转链表

--- leetcode_hot_100/linked/ListNode.java
+++ leetcode_hot_100/linked/ListNode.java
@@ -4,8 +4,15 @@ public class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
---
+++ leetcode_hot_100/linked/Solution106.java
@@ -0,0 +1,33 @@
+package leetcode_hot_100.linked;
+
+public class Solution106 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode Nhead = new ListNode();
+        ListNode pre = head, last = head;
+
+        while (last.next != null) {
+            Nhead.next = last.next;
+            last.next = Nhead.next.next;
+            Nhead.next.next = pre;
+            pre = Nhead.next;
+        }
+
+        return Nhead.next;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
