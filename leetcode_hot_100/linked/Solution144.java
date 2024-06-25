1c3d78e66354b44afcd85988b434f8af3b657d1e
KJay <snoopy_zj@163.com>
2024-06-25 18:10:01 +0800

    环形链表

---
+++ leetcode_hot_100/linked/Solution144.java
@@ -0,0 +1,16 @@
+package leetcode_hot_100.linked;
+
+public class Solution144 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast)
+                return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
