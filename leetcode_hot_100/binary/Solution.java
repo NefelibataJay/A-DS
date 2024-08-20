44cdd510c8cbbd113fb3a55fec2069a616f62246
KJay <snoopy_zj@163.com>
2024-08-20 16:16:08 +0800

    二分搜索

---
+++ leetcode_hot_100/binary/Solution.java
@@ -0,0 +1,69 @@
+package leetcode_hot_100.binary;
+
+class Solution {
+    // 二分搜索
+    public int[] searchRange(int[] nums, int target) {
+        int start = lowerBound(nums, target); // 选择其中一种写法即可
+        if (start == nums.length || nums[start] != target) {
+            return new int[] { -1, -1 }; // nums 中没有 target
+        }
+        // 如果 start 存在，那么 end 必定存在
+        int end = lowerBound(nums, target + 1) - 1;
+        return new int[] { start, end };
+    }
+
+    // 闭区间写法
+    private int lowerBound(int[] nums, int target) {
+        // lowerBound 返回最小的满足 nums[i] >= target 的 i
+        // 如果数组为空，或者所有数都 < target，则返回 nums.length
+        // 要求 nums 是非递减的，即 nums[i] <= nums[i + 1]
+
+        int left = 0, right = nums.length - 1; // 闭区间 [left, right]
+        while (left <= right) { // 区间不为空
+            // 循环不变量：
+            // nums[left-1] < target
+            // nums[right+1] >= target
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1; // 范围缩小到 [mid+1, right]
+            } else {
+                right = mid - 1; // 范围缩小到 [left, mid-1]
+            }
+        }
+        return left;
+    }
+
+    // 左闭右开区间写法
+    private int lowerBound2(int[] nums, int target) {
+        int left = 0, right = nums.length; // 左闭右开区间 [left, right)
+        while (left < right) { // 区间不为空
+            // 循环不变量：
+            // nums[left-1] < target
+            // nums[right] >= target
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1; // 范围缩小到 [mid+1, right)
+            } else {
+                right = mid; // 范围缩小到 [left, mid)
+            }
+        }
+        return left; // 返回 left 还是 right 都行，因为循环结束后 left == right
+    }
+
+    // 开区间写法
+    private int lowerBound3(int[] nums, int target) {
+        int left = -1, right = nums.length; // 开区间 (left, right)
+        while (left + 1 < right) { // 区间不为空
+            // 循环不变量：
+            // nums[left] < target
+            // nums[right] >= target
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid; // 范围缩小到 (mid, right)
+            } else {
+                right = mid; // 范围缩小到 (left, mid)
+            }
+        }
+        return right;
+    }
+}
