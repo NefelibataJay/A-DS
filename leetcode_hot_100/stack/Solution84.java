719156ba8a0f37393ffb8dcfd52e72b4b495f9fd
KJay <snoopy_zj@163.com>
2024-08-27 15:20:05 +0800

    柱状图中最大的矩形

---
+++ leetcode_hot_100/stack/Solution84.java
@@ -0,0 +1,62 @@
+package leetcode_hot_100.stack;
+
+import java.util.*;
+
+public class Solution84 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int largestRectangleArea1(int[] heights) {
+        int n = heights.length;
+        int res = 0;
+        for (int left = 0; left < n; left++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int right = left; right < n; right++) {
+                minHeight = Math.min(minHeight, heights[right]);
+                res = Math.max(res, (right - left + 1) * minHeight);
+            }
+        }
+        return res;
+    }
+
+    public int largestRectangleArea2(int[] heights) {
+        int n = heights.length;
+        int res = 0;
+        for (int mid = 0; mid < n; mid++) {
+            int h = heights[mid];
+            int left = mid, right = mid;
+            while (left > 0 && heights[left - 1] >= h)
+                left--;
+            while (right < n - 1 && heights[right + 1] >= h)
+                right++;
+
+            res = Math.max(res, (right - left + 1) * h);
+        }
+        return res;
+    }
+
+    public int largestRectangleArea3(int[] heights) {
+        int n = heights.length;
+        int res = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        for (int i = 0; i < n; i++) {
+            while (stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
+                int h = heights[stack.pop()];
+                int w = i - 1 - stack.peek();
+                res = Math.max(res, h * w);
+            }
+            stack.push(i);
+        }
+
+        while (stack.peek() != -1) {
+            int h = heights[stack.pop()];
+            int w = n - 1 - stack.peek();
+            res = Math.max(res, h * w);
+        }
+
+        return res;
+    }
+}
