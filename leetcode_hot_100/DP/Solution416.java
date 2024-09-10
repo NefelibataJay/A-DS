8f2b6b3653824687b8792b8b25e5b934857cebfc
KJay <snoopy_zj@163.com>
2024-09-10 16:16:52 +0800

    分割等和子集

--- leetcode_hot_100/DP/Solution416.java
+++ leetcode_hot_100/DP/Solution416.java
@@ -2,6 +2,36 @@ package leetcode_hot_100.DP;
 
 public class Solution416 {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2)
+            return false;
 
+        int sum = 0, maxNum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxNum = Math.max(maxNum, nums[i]);
+        }
+        int target = sum / 2;
+
+        if (sum % 2 != 0 || maxNum > target)
+            return false;
+
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num)
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n - 1][target];
     }
 }
