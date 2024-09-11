de03b5bc2f67d5bbe1c717d62dc552725b3e2cf0
KJay <snoopy_zj@163.com>
2024-09-11 19:46:25 +0800

    update

--- Test/AbstractClass.java
+++ Test/AbstractClass.java
@@ -1,6 +1,9 @@
 package Test;
 
-public abstract class AbstartClass {
+public abstract class AbstractClass {
+    public AbstractClass() {
+    }
+
     // 抽象类可以有定义与实现
     abstract void printString(String str);
 
--- Test/Interface.java
+++ Test/Interface.java
@@ -2,4 +2,19 @@ package Test;
 
 public interface Interface {
     // 接口只有定义，不能有方法的实现
+    void makeSound();
+
+    default void sleep1() {
+        System.out.println("Sleeping1...");
+    }
+
+    static void sleep2() {
+        System.out.println("Sleeping2...");
+    }
+
+    private void sleep3() {
+        // 私有方法不能被实现类访问，只能在接口内部使用
+        System.out.println("Sleeping3...");
+    }
+
 }
