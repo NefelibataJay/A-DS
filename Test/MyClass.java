5a24f1e83c562c68dc7f28800c29e136f51f8376
KJay <snoopy_zj@163.com>
2024-09-12 13:11:42 +0800

    clone

--- Test/MyClass.java
+++ Test/MyClass.java
@@ -1,10 +1,54 @@
 package Test;
 
-public class MyClass {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class MyClass implements Serializable, Cloneable {
     /*
      * 父类静态成员变量、静态代码块（如果有）
      * 子类静态成员变量、静态代码块（如果有）
      * 父类构造方法（实例化对象时）
      * 子类构造方法（实例化对象时）
      */
+
+    private String field;
+    private NestedClass nestedClass;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        MyClass cloned = (MyClass) super.clone();
+        cloned.nestedClass = (NestedClass) nestedClass.clone();
+        return cloned;
+    }
+
+    public MyClass deepCopy() {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+            oos.flush();
+            oos.close();
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            return (MyClass) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
+
+class NestedClass implements Serializable, Cloneable {
+    private int nestedField;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
