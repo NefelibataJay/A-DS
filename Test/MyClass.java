fd276b15b1a9b823be98a678d0b514c9c830c97f
KJay <snoopy_zj@163.com>
2024-09-23 13:22:46 +0800

    update

---
+++ Test/MyClass.java
@@ -0,0 +1,15 @@
+package Test;
+
+/**
+ * MyClass
+ */
+public class MyClass {
+    public MyClass() {
+
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass1 = new MyClass();
+    }
+
+}
