50cc9cd6010a3b99d11ebed435bc4071085d7148
KJay <snoopy_zj@163.com>
2024-12-16 13:47:25 +0800

    update

--- Test/Main.java
+++ Test/Main.java
@@ -1,11 +1,41 @@
 package Test;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String args[]) {
-        while (true) {
-            continue;
-        }
+        new ArrayList<>();
+        new HashMap<>();
+        new HashSet<>();
+
+        new LinkedList<>();
+        new LinkedHashMap<>();
+        new LinkedHashSet<>();
+
+        new TreeMap<>();
+        new TreeSet<>();
+
+        // 每个方法加上 synchronized 关键字
+        new Vector<>();
+        new Hashtable<>();
+
+        new CopyOnWriteArrayList<>();
+
+        // jdk1.7 中加的是分段锁(Segment锁)，也就是锁的粒度更细了。
+        // jdk1.8 中加的是 CAS 和 synchronized 结合的方式, 在元素上加锁。
+        new ConcurrentHashMap<>();
+
+        new CopyOnWriteArraySet<>();
+
+        Collections.synchronizedList(new ArrayList<>());
+
+        new ReentrantLock();
+
+        new CountDownLatch(0);
+
+        new ThreadLocal<>();
     }
+
 }
