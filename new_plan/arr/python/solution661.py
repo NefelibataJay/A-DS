564b9f00dfb60d6bebef1fd29fdf85ded4be00fb
KJay <snoopy_zj@163.com>
2024-12-17 14:29:27 +0800

    图片平滑器

---
+++ new_plan/arr/python/solution661.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def imageSmoother(img: List[List[int]]) -> List[List[int]]:
+    m, n = len(img), len(img[0])
+    ans = [[0] * n for _ in range(m)]
+
+    for i in range(m):
+        for j in range(n):
+            tot, num = 0, 0
+            for x in range(max(i - 1, 0), min(i + 2, m)):
+                for y in range(max(j - 1, 0), min(j + 2, n)):
+                    tot += img[x][y]
+                    num += 1
+            ans[i][j] = tot // num
+
+    return ans
+
+
+img = [[100, 200, 100], [200, 50, 200], [100, 200, 100]]
+
+imageSmoother(img)
